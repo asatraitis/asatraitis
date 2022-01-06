6445c83b55457c412ee9d9f6b3a68a29bc02ce58
Andrius <asatraitis@gmail.com>
2022-01-06 10:13:51 -0500

    Moved links const to config; updated source link to point to repo

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import './footer.css'
+import LINKS from '../../config/links'
 
 export default function Footer() {
-    return <footer className="Footer">source code:<a className="Footer-source-url" href="#">Github</a></footer>
+    return <footer className="Footer">source code:<a className="Footer-source-url" href={LINKS.source}>Github</a></footer>
 }
